1ca29a6ecc199ad38dd88de641ad16d1464de7fb
whenth01 <dodifoxy231@gmail.com>
2026-02-25 20:01:08 +0200

    RNG Menu, persistent history, crash prevention

--- rng.py
+++ rng.py
@@ -1,35 +1,56 @@
+import json
+import random
 rng_history = []
+with open("rng_storage.json", 'r') as f:
+  rng_history = json.load(f)
 def rng_function():
   loop1 = True
   while loop1:
     try:
-      num1 = int(input("Largest number:"))
-      dice = int(input("Dice rolls:"))
-    except:
-      print("Invalid input! only whole numbers are valid.")
+      question = int(input("1(continue), 2(view) or 3(clear) RNG history, or 4(back)?:"))
+      match question:
+        case 1:
+          num1 = input("Smallest number(default is 1):")
+          if num1 == "":
+            num1 = 1
+          else:
+            num1 = int(num1)
+          num2 = int(input("Largest number:"))
+          if num1 > num2:
+            print("Smallest number should be smaller than largest.")
+            continue
+          dice = int(input("Dice rolls:"))
+        case 2:
+          global rng_history
+          for rng in rng_history:
+            print(f"{rng['dice']}d {rng['smallest_number']} - {rng['largest_number']} = {rng['results']}")
+          continue
+        case 3:
+          with open("rng_storage.json", 'w') as f:
+            json.dump([], f)
+          rng_history = []
+          print("RNG History cleared!")
+          continue
+        case 4:
+          loop1 = False
+          break
+    except ValueError:
+      print("Invalid input!")
       continue
+    else:
+      if dice > 1000:
+        print("Max dice rolls is 1000.")
+        continue
     results = []
-    import random
     for i in range(dice):
-      num = random.randint(1,num1)
+      num = random.randint(num1,num2)
       results.append(num)
       print(f"{num}")
     rng_history.append({
-    "amount_rolled": num1,
+    "largest_number": num2,
+    "smallest_number": num1,
     "dice": dice,
     "results": results
     })    
-    while True:
-      question = int(input("Restart rng(1), view rng history(2), or quit(3)?:"))
-      if question == 1:
-        break
-      elif question == 2:
-        for rng in rng_history:
-          print(f"{rng['dice']}d{rng['amount_rolled']} = {rng['results']}")
-        continue
-      elif question == 3:
-        loop1 = False
-        break
-      else:
-        print("Invalid. Please input a valid entry.")
-        continue
+    with open("rng_storage.json", 'w') as f:
+      json.dump(rng_history, f, indent=2) 
