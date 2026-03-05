b1ca96d234a726960a381e33a8960e47ccd64765
whenth01 <dodifoxy231@gmail.com>
2026-03-05 07:52:54 +0200

    Update rng.py

--- rng.py
+++ rng.py
@@ -1,14 +1,16 @@
-import json
-import random
-rng_history = []
-with open("rng_storage.json", 'r') as f:
-  rng_history = json.load(f)
-def rng_function():
-  loop1 = True
-  while loop1:
+# rng
+import json,random
+if __name__ == "__main__":
+  print("Run calculator_v3.py for it to work.")
+
+# main function
+def rng_function(rng_history):
+  while True:
     try:
-      question = int(input("1(continue), 2(view) or 3(clear) RNG history, or 4(back)?:"))
-      match question:
+      choice = int(input("1(continue), 2(view) or 3(clear) RNG history, or 4(back)?:"))
+      match choice:
+
+# user inputs for RNG
         case 1:
           num1 = input("Smallest number(default is 1):")
           if num1 == "":
@@ -20,32 +22,44 @@ def rng_function():
             print("Smallest number should be smaller than largest.")
             continue
           dice = int(input("Dice rolls:"))
+
+# history viewer
         case 2:
-          global rng_history
-          for rng in rng_history:
-            print(f"{rng['dice']}d {rng['smallest_number']} - {rng['largest_number']} = {rng['results']}")
+          for hist in rng_history:
+            print(f"{hist['dice']}d {hist['smallest_number']} - {hist['largest_number']} = {hist['results']}")
+          if len(rng_history) < 1:
+            print("History is empty.")
           continue
+
+# history deletion
         case 3:
           with open("rng_storage.json", 'w') as f:
             json.dump([], f)
-          rng_history = []
+          rng_history.clear()
           print("RNG History cleared!")
           continue
+
+# return to calculator
         case 4:
-          loop1 = False
           break
+
+
     except ValueError:
       print("Invalid input!")
       continue
     else:
-      if dice > 1000:
-        print("Max dice rolls is 1000.")
+      if dice > 1000 or dice < 1:
+        print("Max dice rolls is 1000, minimum is 1.")
         continue
     results = []
+    
+# number generation
     for i in range(dice):
       num = random.randint(num1,num2)
       results.append(num)
       print(f"{num}")
+
+# history writing
     rng_history.append({
     "largest_number": num2,
     "smallest_number": num1,
@@ -54,3 +68,15 @@ def rng_function():
     })    
     with open("rng_storage.json", 'w') as f:
       json.dump(rng_history, f, indent=2) 
+
+# what calculator calls to run the rng
+def run():
+# creates a new history file if it dosent exist
+  rng_history = []
+  try:
+    with open("rng_storage.json", 'r') as f:
+      rng_history = json.load(f)
+  except (FileNotFoundError,json.JSONDecodeError):
+    with open("rng_storage.json", "w") as f:
+      json.dump(rng_history, f, indent=2)
+  rng_function(rng_history)
